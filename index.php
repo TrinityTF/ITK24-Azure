f486c1868dfbe19a80584df6a62334995bc9fb45
Agu Toomas Pihelgas <68427806+TrinityTF@users.noreply.github.com>
2025-03-14 14:55:05 +0200

    Update index.php
    added background image

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     <head>
         <title>ITK24 Azurei App</title>
     </head>
-    <body>
-      <p>Test</p>
+    <body style="background-image: url('https://images.hdqwalls.com/wallpapers/waterfall-rocks-4k-ch.jpg');">
+        <h1>ITK24 Azurei Project</h1>
+        <p>Test</p>
     </body>
 </html>
