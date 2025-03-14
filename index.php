7fb56fbe6beb6fdaf86b411ceea2b11ebc8ba881
Agu Toomas Pihelgas <68427806+TrinityTF@users.noreply.github.com>
2025-03-14 15:05:23 +0200

    Update index.php
    added admin link

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
     <head>
         <title>ITK24 Azurei App</title>
     </head>
-    <body style="background-image: url('https://images.hdqwalls.com/wallpapers/waterfall-rocks-4k-ch.jpg');">
+    <body>
         <h1>ITK24 Azurei Project</h1>
         <p>Test</p>
+        <img src="https://images.hdqwalls.com/wallpapers/waterfall-rocks-4k-ch.jpg"></img>
+        <a href="https://portal.azure.com/#@followercase.com/resource/subscriptions/531371bc-4ac6-4729-a4fe-8ded1d4fedce/resourcegroups/ITK24-AguToomasPihelgas/providers/Microsoft.Web/sites/aguApp/appServices">Azure Admin Console</a>
     </body>
 </html>
