d192a7d7d51da479c49c46200402eae2ff4d5e36
Agu Toomas Pihelgas <68427806+TrinityTF@users.noreply.github.com>
2025-03-14 15:10:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body>
         <h1>ITK24 Azurei Project</h1>
         <p>Test</p>
-        <img src="https://images.hdqwalls.com/wallpapers/waterfall-rocks-4k-ch.jpg"></img>
+        <img src="https://images.hdqwalls.com/wallpapers/waterfall-rocks-4k-ch.jpg" width="500" height="600"></img>
         <a href="https://portal.azure.com/#@followercase.com/resource/subscriptions/531371bc-4ac6-4729-a4fe-8ded1d4fedce/resourcegroups/ITK24-AguToomasPihelgas/providers/Microsoft.Web/sites/aguApp/appServices">Azure Admin Console</a>
     </body>
 </html>
